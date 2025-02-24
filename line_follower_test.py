c059d95015d9f8527344dc98bb3d3253b657141b
ks2134 <ks2134@cam.ac.uk>
2025-02-24 15:51:41 +0000

    Working basic line follower code

--- line_follower_test.py
+++ line_follower_test.py
@@ -22,45 +22,50 @@ class Motor:
     #self.pwm1.duty_u16(int(65535*30/100))
     self.pwm1.duty_u16(int(65535*speed/100))
 
-class TrackSensor(self, pin):
+
+class TrackSensor:
     def __init__(self, pin):
-        self.pin = Pin(15, Pin.IN, Pin.PULL_DOWN)
-        self.value = self.pin.value()
+        self.sensor_pin = Pin(pin, Pin.IN, Pin.PULL_DOWN)
+    
+    def reading(self):
+        return self.sensor_pin.value()
+    
 
-motor1 = Motor(7,6)
-motor2 = Motor(4,5)
-sensor1 = TrackSensor(15)
-sensor2 = TrackSensor(14)
-speed = 30
-
-def forward(motor1, motor2, speed):
+def forward(speed):
     motor1.Forward(speed)
     motor2.Forward(speed)
 
-def reverse(motor1,motor2, speed):
+def reverse(speed):
     motor1.Reverse(speed)
     motor2.Reverse(speed)
 
-def rightTurn(motor1,motor2):
+def rightTurn():
     motor1.Forward(30)
     motor2.Reverse(30)
 
-def leftTurn(motor1,motor2):
+def leftTurn():
     motor1.Reverse(30)
     motor2.Forward(30)
 
-def stop(motor1, motor2):
+def stop():
     motor1.off()
     motor2.off()
+    
+motor1 = Motor(7,6)
+motor2 = Motor(4,5)
+sensor_left = TrackSensor(15)
+sensor_right = TrackSensor(14)
+speed = 75
 
 while True:
-    right_val = sensor1.value()
-    left_val = sensor2.value()
+    right_val = sensor_right.reading()
+    left_val = sensor_left.reading()
+    
     #right_val=right_ir.value() #Getting right IR value(0 or 1)
     #left_val=left_ir.value() #Getting left IR value(0 or 1)
     # Controlling robot direction based on IR value
     if right_val==0 and left_val==0:
-        forward()
+        forward(speed)
     elif right_val==1 and left_val==0:
         rightTurn()
     elif right_val==0 and left_val==1:
