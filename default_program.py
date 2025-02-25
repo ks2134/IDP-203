1cd40de76bf1ddebbef1a8d1b942eb099adcbd8d
ElviraMunte <emunteanu@perse.co.uk>
2025-02-25 10:55:19 +0000

    trying to move on to 4 sensors, not working yet

--- default_program.py
+++ default_program.py
@@ -1,78 +1,74 @@
-from machine import Pin , PWM
-from utime import sleep
-
-# Set up PWM Pin for servo control
-servo1_pin = machine.Pin(13)
-servo1 = PWM(servo1_pin)
-
-# Set Duty Cycle for Different Angles
-max_duty = 7864
-min_duty = 1802
-half_duty = int(max_duty/2)
-
-#Set PWM frequency
-frequency = 50
-servo1.freq (frequency)
-
-dir1 = Pin(0,Pin.OUT)
-pwm1 = PWM(Pin(1))
-pwm1.freq(1000)
-
-dir3 = Pin(4,Pin.OUT)
-pwm3 = PWM(Pin(5))
-pwm3.freq(1000)
-
-def PushOut(duty):
-    dir1.value(1)
-    duty_16 = int((duty*65536)/100)
-    pwm1.duty_u16(duty_16)
+from machine import Pin, PWM
+from time import sleep
+class Motor:
+ def __init__(self, pin1, pin2):
+    #self.m1Dir = Pin(7 , Pin.OUT) # set motor direction
+    self.m1Dir = Pin(pin1 , Pin.OUT) # set motor direction
+    #self.pwm1 = PWM(Pin(6)) # set speed
+    self.pwm1 = PWM(Pin(pin2)) # set speed
+    self.pwm1.freq(1000) # set max frequency
+    self.pwm1.duty_u16(0) # set duty cycle
     
-def PullIn(duty):
-    dir1.value(0)
-    duty_16 = int((duty*65536)/100)
-    pwm1.duty_u16(duty_16)
-    
-def StopLinearAc():
-    dir1.value(0)
-    pwm1.duty_u16(0)
+ def off(self):
+    self.pwm1.duty_u16(0)
 
-def RotateCW(duty):
-    dir3.value(1)
-    duty_16 = int((duty*65536)/100)
-    pwm3.duty_u16(duty_16)
+ def Forward(self, speed):
+    self.m1Dir.value(0) # forward = 0 reverse = 1 motor 1
+    #self.pwm1.duty_u16(int(65535*100/100)) # speed range 0-100 motor 1
+    self.pwm1.duty_u16(int(65535*speed/100)) # speed range 0-100 motor 1
 
-def RotateCCW(duty):
-    dir3.value(0)
-    duty_16 = int((duty*65536)/100)
-    pwm3.duty_u16(duty_16)
+ def Reverse(self, speed):
+    self.m1Dir.value(1)
+    #self.pwm1.duty_u16(int(65535*30/100))
+    self.pwm1.duty_u16(int(65535*speed/100))
+
+
+class TrackSensor:
+    def __init__(self, pin):
+        self.sensor_pin = Pin(pin, Pin.IN, Pin.PULL_DOWN)
     
-def StopMotor():
-    dir3.value(0)
-    pwm3.duty_u16(0) 
+    def reading(self):
+        return self.sensor_pin.value()
+    
+
+def forward(speed):
+    motor1.Forward(speed)
+    motor2.Forward(speed)
+
+def reverse(speed):
+    motor1.Reverse(speed)
+    motor2.Reverse(speed)
+
+def rightTurn():
+    motor1.Forward(speed)
+    motor2.Forward(0.3*speed)
+
+def leftTurn():
+    motor1.Forward(0.3*speed)
+    motor2.Forward(speed)
+
+def stop():
+    motor1.off()
+    motor2.off()
+    
+motor1 = Motor(7,6)
+motor2 = Motor(4,5)
+sensor_left = TrackSensor(15)
+sensor_right = TrackSensor(14)
+speed = 75
 
 while True:
-    duty_cycle=(100)
-    print (duty_cycle)
-    PushOut(duty_cycle)
-    sleep(8)
-    PullIn(duty_cycle)
-    sleep(8)
-    StopLinearAc()
-    sleep(1)
-    RotateCW(duty_cycle)
-    sleep(5)
-    RotateCCW(duty_cycle)
-    sleep(5)
-    StopMotor()
-    sleep(1)
-    #Servo at 0 degrees
-    servo1.duty_u16(min_duty)
-    sleep(2)
-    #Servo at 90 degrees
-    servo1.duty_u16(half_duty)
-    sleep(2)
-    #Servo at 180 degrees
-    servo1.duty_u16(max_duty)
-    sleep(2) 
-
-  
+    right_val = sensor_right.reading()
+    left_val = sensor_left.reading()
+    
+    #right_val=right_ir.value() #Getting right IR value(0 or 1)
+    #left_val=left_ir.value() #Getting left IR value(0 or 1)
+    # Controlling robot direction based on IR value
+    if right_val==0 and left_val==0:
+        forward(speed)
+    elif right_val==1 and left_val==0:
+        rightTurn()
+    elif right_val==0 and left_val==1:
+        leftTurn()
+    else:
+        stop()
--- line_follower_test.py
+++ line_follower_test.py
@@ -1,5 +1,5 @@
 from machine import Pin, PWM
-from utime import sleep
+from time import sleep
 class Motor:
  def __init__(self, pin1, pin2):
     #self.m1Dir = Pin(7 , Pin.OUT) # set motor direction
@@ -22,7 +22,7 @@ class Motor:
     #self.pwm1.duty_u16(int(65535*30/100))
     self.pwm1.duty_u16(int(65535*speed/100))
 
-
+#line is 2.5cm wide
 class TrackSensor:
     def __init__(self, pin):
         self.sensor_pin = Pin(pin, Pin.IN, Pin.PULL_DOWN)
@@ -39,13 +39,13 @@ def reverse(speed):
     motor1.Reverse(speed)
     motor2.Reverse(speed)
 
-def rightTurn():
-    motor1.Forward(30)
-    motor2.Reverse(30)
+def rightTurn(f, speed):
+    motor1.Forward(speed)
+    motor2.Forward(f*speed)
 
-def leftTurn():
-    motor1.Reverse(30)
-    motor2.Forward(30)
+def leftTurn(f, speed):
+    motor1.Forward(f*speed)
+    motor2.Forward(speed)
 
 def stop():
     motor1.off()
@@ -55,20 +55,31 @@ motor1 = Motor(7,6)
 motor2 = Motor(4,5)
 sensor_left = TrackSensor(15)
 sensor_right = TrackSensor(14)
-speed = 75
+sensor_Tleft = TrackSensor(13)
+sensor_Tright = TrackSensor(12)
+speed1 = 75
+speed2 = 30
+f1 = 0.7 #good for small corrections when straight lining
+f2 = 0.3
 
 while True:
     right_val = sensor_right.reading()
     left_val = sensor_left.reading()
+    Tright_val = sensor_Tright.reading()
+    Tleft_val = sensor_Tleft.reading()
     
     #right_val=right_ir.value() #Getting right IR value(0 or 1)
     #left_val=left_ir.value() #Getting left IR value(0 or 1)
     # Controlling robot direction based on IR value
-    if right_val==0 and left_val==0:
-        forward(speed)
-    elif right_val==1 and left_val==0:
-        rightTurn()
-    elif right_val==0 and left_val==1:
-        leftTurn()
+
+    if (Tright_val and Tleft_val) == 0: #straight line
+        if right_val==0 and left_val==0:
+            forward(speed1)
+        elif right_val==1 and left_val==0:
+            rightTurn(f1,speed1)
+        elif right_val==0 and left_val==1:
+            leftTurn(f1,speed1)
+        else:
+            stop()
     else:
         stop()
