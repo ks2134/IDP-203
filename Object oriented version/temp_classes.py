757ae96b5385d9a4715fd6a58e00354808c3fa48
ElviraMunte <emunteanu@perse.co.uk>
2025-03-18 16:31:05 +0000

    final changes

--- Object oriented version/temp_classes.py
+++ Object oriented version/temp_classes.py
@@ -1,12 +1,35 @@
 from machine import Pin, PWM, I2C
 from time import sleep, time
 from colour_sensor import TCS34725
-from distance_sensor import VL53L0X
+#from distance_sensor import VL53L0X
 
 
 #Driving motor on robot. Inputs: pin1 = motor direction, pin2 = pwm pin
 class Motor:
-   def __init__(self, dir_pin, pwm_pin):
+   """
+   Description
+   -----------
+   Class representing one of the driving motors on the robot and sets up control for the two directions the robot can travel in 
+   (Forward and Reverse).
+   
+   Attributes
+   ----------
+   dir_pin : int
+      Direction control pin for the motor (see pinout diagram for Pi HAT for the correct pin for each motor)
+   pwm_pin : int
+      PWM pin for speed control for the motor (again see pinout diagram for the correct pin values)
+   
+   Methods
+   -------
+   off()
+      Turn motor off
+   Forward(speed: int)
+      Drive motor in the forward direction at specified speed (0 to 100)
+   Reverse(speed: int)
+      Drive motor in reverse direction at specified speed (0 to 100)   
+   """
+
+   def __init__(self, dir_pin: int, pwm_pin: int):
       #self.m1Dir = Pin(7 , Pin.OUT) # set motor direction
       self.m1Dir = Pin(dir_pin , Pin.OUT) # set motor direction
       #self.pwm1 = PWM(Pin(6)) # set speed
@@ -15,31 +38,101 @@ class Motor:
       self.pwm1.duty_u16(0) # set duty cycle
       
    def off(self):
+      """Turn off the motor (sets duty cycle to 0)"""
       self.pwm1.duty_u16(0)
 
-   def Forward(self, speed):
+   def Forward(self, speed: int):
+      """Drives motor in forward direction at specified speed (0 to 100)"""
       self.m1Dir.value(1) # forward = 1 reverse = 0 motor 1
       #self.pwm1.duty_u16(int(65535*100/100)) # speed range 0-100 motor 1
       self.pwm1.duty_u16(int(65535*speed/100)) # speed range 0-100 motor 1
 
-   def Reverse(self, speed):
+   def Reverse(self, speed: int):
+      """Drives motor in reverse direction at specified speed (0 to 100)"""
       self.m1Dir.value(0)
       #self.pwm1.duty_u16(int(65535*30/100))
       self.pwm1.duty_u16(int(65535*speed/100))
 
 
 class TrackSensor:
-   def __init__(self, pin):
+   """
+   Description
+   -----------
+   Class representing a line sensor on the robot, which is used for general line following, or for detecting and implimenting turns.
+
+   Attributes
+   ----------
+   pin: int
+      GPIO Pin the data output of the sensor is connected to.
+
+   Methods
+   -------
+   reading() -> int
+      Returns the sensor reading at that instant (1 - white, 0 - black)
+   """
+   def __init__(self, pin: int):
       self.sensor_pin = Pin(pin, Pin.IN, Pin.PULL_DOWN)
 
-   def reading(self):
+   def reading(self) -> int:
+      """Return current value of sensor: 1 - white, 0 - black"""
       return self.sensor_pin.value()
    
    
 class Vehicle:
-   def __init__(self, left_motor_dir, left_motor_pwm, right_motor_dir, right_motor_pwm,
-                 left_track, right_track, Tleft_track, Tright_track, led_pin, speed, 
-                 i2c0_sda, i2c0_scl, i2c0_bus_no, max_servo_pos, min_servo_pos, servo_pin, button_pin):
+   """
+   Description
+   -----------
+   Class used to set up a Vehicle (whole robot).
+
+   Attributes
+   ----------
+   left_motor_dir: int
+      Direction GPIO pin number for left motor
+   left_motor_pwm: int
+      PWM GPIO pin number for left motor
+   right_motor_dir: int
+      Direction GPIO pin number for right motor
+   right_motor_pwm: int
+      PWM GPIO pin number for right motor
+   
+   left_track: int
+      GPIO Pin for inner left line sensor
+   right_track: int
+      GPIO Pin for inner right line sensor
+   Tleft_track: int
+      GPIO Pin for outer left line sensor
+   Tright_track: int
+      GPIO Pin for outer right line sensor
+
+   led_pin: int
+      GPIO Pin to output to flashing LED subsystem. Set high or low to turn flashing on/off respectively.
+   speed: int
+      Speed value for robot (range 0-100). Class accounts for balancing of motors.
+   i2c0_sda: int
+      SDA pin used for I2C bus 0 (this is used for colour sensor)
+   i2c0_scl: int
+      SCL pin used for I2C bus 0 (this is used for colour sensor)
+   i2c0_bus_no: int
+      I2C bus being used for colour sensor (set as 0 in this case)
+
+   max_servo_pos: int
+      Servo position value for upper setting - used when carrying box.
+   min_servo_pos: int
+      Servo position value for lower setting - used when picking/dropping off box.
+   servo_pin: int
+      Data PWM pin for the servo.
+   
+   button_pin: int
+      GPIO pin for push button input.
+   
+   Methods
+   -------
+
+   """
+   def __init__(self, left_motor_dir:int, left_motor_pwm:int, right_motor_dir:int, right_motor_pwm:int,
+                 left_track:int, right_track:int, Tleft_track:int, Tright_track:int, led_pin:int, speed:int, 
+                 i2c0_sda:int, i2c0_scl:int, i2c0_bus_no:int, max_servo_pos:int, min_servo_pos:int, servo_pin:int, 
+                 button_pin:int):
       
       #Setting up driving motors
       self.left_motor = Motor(left_motor_dir, left_motor_pwm)
@@ -67,17 +160,16 @@ class Vehicle:
       self.max_servo_pos = max_servo_pos
       self.min_servo_pos = min_servo_pos
       self.servo = PWM(Pin(servo_pin))
-      self.servo.freq(50)        #PWM Frequency set at 50Hz
+      self.servo.freq(50)                 #PWM Frequency set at 50Hz
 
       #Setting up distance sensor
       #self.i2c1_bus = I2C(i2c1_bus_no, sda=i2c1_sda, scl=i2c1_scl)
       #self.distance_sensor = VL53L0X(self.i2c1_bus)
       # self.distance_sensor.set_measurement_timing_budget(40000)
       # self.distance_sensor.set_Vcsel_pulse_period(self.distance_sensor.vcsel_period_type[0], 8)
-
       
-
-   def forward(self, getting_box=1, previous_state=[0, 0], state_counter=0):
+   #Move both motors in forward direction.
+   def forward(self, getting_box=1, previous_state: list = [0, 0], state_counter=0):
       
       if previous_state != [0, 0]:
          state_counter = 0
@@ -87,13 +179,14 @@ class Vehicle:
       
       return state_counter
       
-
+   #Move both motors in reverse direction.
    def reverse(self):
       self.left_motor.Reverse(self.left_motor_speed)
       self.right_motor.Reverse(self.right_motor_speed)
 
-
-   def leftTurn(self, previous_state, state_counter, state_counter_trip, line_correction, F1_ORIGINAL, current_f, getting_box):
+   #Slight adjustment left for line following.
+   def leftTurn(self, previous_state:list, state_counter:int, state_counter_trip:int, line_correction:int,
+                 F1_ORIGINAL:float, current_f:float, getting_box:float):
 
       if previous_state != [0, 1]:
          state_counter = 0
@@ -111,8 +204,9 @@ class Vehicle:
 
       return state_counter, current_f
 
-
-   def rightTurn(self, previous_state, state_counter, state_counter_trip, line_correction, F1_ORIGINAL, current_f, getting_box):
+   #Slight adjustment right for line following.
+   def rightTurn(self, previous_state:list, state_counter:int, state_counter_trip:int, line_correction:int,
+                  F1_ORIGINAL:float, current_f:float, getting_box:float):
 
       if previous_state != [1, 0]:
          state_counter = 0
@@ -130,30 +224,34 @@ class Vehicle:
 
       return state_counter, current_f
 
+   #Major turn to the left.
+   def leftPivot(self, f:float, previous_state:list):
 
-   def leftPivot(self, f, previous_state):
       self.right_motor.Forward(self.right_motor_speed)
       self.left_motor.Reverse(f * self.left_motor_speed)
 
       previous_state = [1, 1]
       return previous_state
 
+   #Major turn to the right.
+   def rightPivot(self, f:float, previous_state:list):
 
-   def rightPivot(self, f, previous_state):
       self.right_motor.Reverse(f * self.right_motor_speed)
       self.left_motor.Forward(self.left_motor_speed)
 
       previous_state = [1, 1]
       return previous_state
 
-
+   #Bring robot to complete halt.
    def stop(self):
       self.right_motor.off()
       self.left_motor.off()
                
    
-   #Normal forward driving on line following
-   def go_forward(self, previous_state, F1_ORIGINAL, current_f, state_counter, line_correction, state_counter_trip, getting_box):
+   #Normal forward driving algorithm with line following
+   def go_forward(self, previous_state:list, F1_ORIGINAL:float, current_f:float, state_counter:int, line_correction:int,
+                   state_counter_trip:int, getting_box:float):
+      
       right_val = self.sensor_right.reading()
       left_val = self.sensor_left.reading()
 
@@ -175,11 +273,11 @@ class Vehicle:
          state_counter = self.forward(getting_box, previous_state, state_counter)
 
       return previous_state, state_counter, current_f
-
    
    #Turning right on a corner (CR)
-   def turn_Cright(self, f, previous_state):
+   def turn_Cright(self, f:float, previous_state:list):
 
+      sleep(0.1)
       left_val = self.sensor_left.reading()
       while (left_val == 1):
          left_val = self.sensor_left.reading()
@@ -192,8 +290,9 @@ class Vehicle:
       return previous_state
 
    #Turning left on a corner (CL)
-   def turn_Cleft(self, f, previous_state): #corner
+   def turn_Cleft(self, f:float, previous_state:list): #corner
 
+      sleep(0.1)
       right_val = self.sensor_right.reading()
       while (right_val == 1):
          right_val = self.sensor_right.reading()
@@ -204,11 +303,9 @@ class Vehicle:
          previous_state = self.leftPivot(f, previous_state)
 
       return previous_state
-
-
          
    #Turning right on a 'sideways T' (R)
-   def turn_right(self, f, previous_state):
+   def turn_right(self, f:float, previous_state:list):
 
       Tleft_val = self.sensor_Tleft.reading()
       left_val = self.sensor_left.reading()
@@ -232,7 +329,7 @@ class Vehicle:
       return previous_state
 
    #Turning left on a 'sideways T' (L)
-   def turn_left(self, f, previous_state):
+   def turn_left(self, f:float, previous_state:list):
 
       Tright_val = self.sensor_Tright.reading()
       right_val = self.sensor_right.reading()
@@ -256,7 +353,7 @@ class Vehicle:
       return previous_state
 
    #Turning right on 'head-on T' (TR)
-   def turn_Tright(self, f, previous_state):
+   def turn_Tright(self, f:float, previous_state:list):
       left_val = self.sensor_left.reading()
       
       while (left_val == 1):
@@ -270,7 +367,7 @@ class Vehicle:
       return previous_state
 
    #Turning left on 'head-on T' (TL)
-   def turn_Tleft(self, f, previous_state): 
+   def turn_Tleft(self, f:float, previous_state:list): 
       right_val = self.sensor_right.reading()
       
       while (right_val == 1):
@@ -283,8 +380,11 @@ class Vehicle:
 
       return previous_state
 
+
    #Continuing straight and ignoring any alternate paths (S)
-   def ignore(self, previous_state, F1_ORIGINAL, current_f, state_counter, line_correction, state_counter_trip, getting_box):
+   def ignore(self, previous_state:list, F1_ORIGINAL:float, current_f:float, state_counter:int, line_correction:int,
+              state_counter_trip:int, getting_box:float):
+      
       Tleft_val = self.sensor_Tleft.reading()
       Tright_val = self.sensor_Tright.reading()
 
@@ -294,8 +394,8 @@ class Vehicle:
 
          self.go_forward(previous_state, F1_ORIGINAL, current_f, state_counter, line_correction, state_counter_trip, getting_box)
 
-   #Go back and drive off left
-   def reverse_left(self, f, previous_state):
+   #Go back and drive off facing left after picking up box
+   def reverse_left(self, f:float, previous_state:list):
       right_val = self.sensor_right.reading()
 
       while (right_val == 0):
@@ -312,8 +412,8 @@ class Vehicle:
 
       return previous_state
 
-   #Go back and drive off right
-   def reverse_right(self, f, previous_state):
+   #Go back and drive off facing right after picking up box
+   def reverse_right(self, f:float, previous_state:list):
       left_val = self.sensor_left.reading()
 
       while (left_val == 0):
@@ -331,7 +431,7 @@ class Vehicle:
       return previous_state
 
    #After dropping off the box, perform 180 degree turn to the left.
-   def spin_left(self, f, previous_state):
+   def spin_left(self, f:float, previous_state:list):
       state_counter = self.forward()
       sleep(0.5)
       self.servo.duty_u16(self.min_servo_pos) #drops box
@@ -352,7 +452,7 @@ class Vehicle:
       return previous_state
 
    #After dropping off the box, perform 180 degree turn to the right.
-   def spin_right(self, f, previous_state):
+   def spin_right(self, f:float, previous_state:list):
       state_counter = self.forward()
       sleep(0.5)
       self.servo.duty_u16(self.min_servo_pos) #drops box
@@ -372,19 +472,21 @@ class Vehicle:
 
       return previous_state
 
-   def box_right(self,f, previous_state, box_num):
+   #Turning in right to pick up box.
+   def box_right(self, f:float, previous_state:list):
+
       self.forward(previous_state)
-      
-      sleep(0.28)
+      sleep(0.32)
 
       previous_state = self.rightPivot(f, previous_state)
-      sleep(0.7)
+      sleep(0.75)
 
       return previous_state
 
-   def box_left(self, f, previous_state, box_num):    
-      self.forward(previous_state)
+   #Turning in left to pick up box.
+   def box_left(self, f:float, previous_state:list):    
       
+      self.forward(previous_state)
       sleep(0.28)
 
       previous_state = self.leftPivot(f, previous_state)
@@ -392,58 +494,9 @@ class Vehicle:
 
       return previous_state
 
-   #Picking up the box. (Takes f just to keep loop general)
-   def box1(self, f):
-      Tleft_val = self.sensor_Tleft.reading()
-      Tright_val = self.sensor_Tright.reading()
 
-      while ((Tleft_val == 1) or (Tright_val == 1)):
-         Tleft_val = self.sensor_Tleft.reading()
-         Tright_val = self.sensor_Tright.reading()
-
-         self.reverse()
-    
-   def box(self):
-      pass
-      
-   #get_box using Distance sensor
-   def get_box1(self, previous_state, F1_ORIGINAL, current_f, state_counter, line_correction, state_counter_trip, getting_box):
-         
-         passed_node = False
-         found_box = False
-         Tleft_val = self.sensor_Tleft.reading()
-         Tright_val = self.sensor_Tright.reading()
-         while(found_box == False):
-            #print(self.distance_sensor.ping()-50,"mm")
-            distance = self.distance_sensor.ping()-40
-            
-            if (distance < 15):
-               found_box = True
-               #sleep(0.5)
-            Tleft_val = self.sensor_Tleft.reading()
-            Tright_val = self.sensor_Tright.reading()
-            previous_state, state_counter, current_f = self.go_forward(previous_state, F1_ORIGINAL, current_f, state_counter, line_correction, state_counter_trip, getting_box)
-            if ((Tleft_val == 1) or (Tright_val == 1)):
-               passed_node = True
-         self.servo.duty_u16(self.max_servo_pos) #picks up box
-         self.stop()
-         sleep(2)
-         RGB_inc = self.get_colour() #scans box colour
-         if (passed_node == True):
-            Tleft_val = self.sensor_Tleft.reading()
-            Tright_val = self.sensor_Tright.reading()
-            while ((Tleft_val == 0) and (Tright_val == 0)):
-               Tleft_val = self.sensor_Tleft.reading()
-               Tright_val = self.sensor_Tright.reading()
-               self.reverse()
-            while ((Tleft_val == 1) and (Tright_val == 1)):
-               Tleft_val = self.sensor_Tleft.reading()
-               Tright_val = self.sensor_Tright.reading()
-               self.reverse()
-         return RGB_inc
-      
-   #Get box without using distance sensor.
-   def get_box(self):
+   #Pick up box without using distance sensor.
+   def get_box(self) -> int:
       #print("get box func")
       self.servo.duty_u16(self.max_servo_pos) #picks up box
       self.stop()
@@ -459,9 +512,11 @@ class Vehicle:
          Tleft_val = self.sensor_Tleft.reading()
          Tright_val = self.sensor_Tright.reading()
          self.reverse()
+
       return RGB_inc
-            
-   def get_colour(self): #under construction
+   
+   #Take 5 colour sensor readings, average them, and decide which colour box it.
+   def get_colour(self) -> int:
       #[red, green, blue]
       colour_values = [[], [], []]
 
@@ -515,8 +570,11 @@ class Vehicle:
 
       #Starting position manoeuvring
    
-   
-   def start(self, previous_state, F1_ORIGINAL, current_f, state_counter, line_correction, state_counter_trip, getting_box):
+
+   #Detect and move outside the starting zone, onto track.
+   def start(self, previous_state:list, F1_ORIGINAL:float, current_f:float, state_counter:int, line_correction:int,
+              state_counter_trip:int, getting_box:float):
+      
       Tleft_val = self.sensor_Tleft.reading()
       Tright_val = self.sensor_Tright.reading()
 
@@ -537,8 +595,10 @@ class Vehicle:
 
       #End up back in the box.
    
-   
-   def finish(self, previous_state, F1_ORIGINAL, current_f, state_counter, line_correction, state_counter_trip, getting_box):
+   #Come off the track and stop in starting zone.
+   def finish(self, previous_state:list, F1_ORIGINAL:float, current_f:float, state_counter:int, line_correction:int,
+               state_counter_trip:int, getting_box:float):
+      
       Tleft_val = self.sensor_Tleft.reading()
       Tright_val = self.sensor_Tright.reading()
 
@@ -553,7 +613,8 @@ class Vehicle:
 
       return previous_state, state_counter, current_f
 
-   #Node detection function
+
+   #Identify whether a node on the track has been passed.
    def detect_node(self):
       Tleft_val = self.sensor_Tleft.reading()
       Tright_val = self.sensor_Tright.reading()
--- Object oriented version/temp_main_program.py
+++ Object oriented version/temp_main_program.py
@@ -1,47 +1,52 @@
-from time import sleep
 from classes import *
 from route_tree import *
 
-#Constants and definitions:
+#----------------------------Constants and definitions:----------------------------#
+#Motor pins.
 LEFT_MOTOR_DIR_PIN = 7
 LEFT_MOTOR_PWM_PIN = 6
-
-LEFT_MOTOR_SPEED = 100
-RIGHT_MOTOR_SPEED = 98
 
 RIGHT_MOTOR_DIR_PIN = 4
 RIGHT_MOTOR_PWM_PIN = 5
 
+#Individual speeds for balancing.
+LEFT_MOTOR_SPEED = 100
+RIGHT_MOTOR_SPEED = 98
+
+#Line following sensor pins.
 OUTER_LEFT_TRACK_SENSOR_PIN = 26
 LEFT_TRACK_SENSOR_PIN = 20
 RIGHT_TRACK_SENSOR_PIN = 11
 OUTER_RIGHT_TRACK_SENSOR_PIN = 8
 
+#Pin definition for LED and Start/Stop button.
 LED_PIN = 9
 BUTTON_PIN = 12
 
+#Motor speed ratios.
 F1_ORIGINAL = 0.7
 F2_ORIGINAL = 0.5
 F3_ORIGINAL = 1.0
 
+#Line following proportional control parameters.
 LINE_CORRECTION = 0.2
 STATE_COUNTER_TRIP = 500 
 
-#Motor speed
+#Overall robot speed setting, and box collection speed setting.
 SPEED = 100
 BOX_SPEED_COEFFICIENT = 1
 
-#Colour sensor I2C bus
+#Colour sensor I2C bus setup.
 I2C0_SDA = 16
 I2C0_SCL = 17
 I2C0_BUS_NO = 0
 
-#Servo parameters
+#Servo postition parameters, and pin setup.
 SERVO_0 = 1850
 SERVO_10 = 1400 
 SERVO_PIN = 15
 
-#Time of Flight sensor I2C bus
+#Time of Flight sensor I2C bus setup (there if needed).
 I2C1_SDA = 18
 I2C1_SCL = 19
 I2C1_BUS_NO = 1
@@ -60,13 +65,12 @@ state_counter = 0
 current_f = F1_ORIGINAL
 button_stop = False
 
-def button_callback(button):
-    global button_stop
-    button_stop = True
-
 
 while robot.button.value() != 1:
     pass
+
+
+#--------------------------------------------------------Main loop:--------------------------------------------------------#
 
 
 node_types = {"L":robot.turn_left, "R":robot.turn_right, "S":robot.ignore, "CR":robot.turn_Cright, "CL":robot.turn_Cleft,
@@ -74,9 +78,8 @@ node_types = {"L":robot.turn_left, "R":robot.turn_right, "S":robot.ignore, "CR":
               "SR":robot.spin_right, "SL":robot.spin_left, "B":robot.get_box,"FIN":robot.finish, "BR":robot.box_right,
               "BL":robot.box_left}
 
-#S for straight, CR CL for corners, TL TR for 'head on' t, L and R for side t
 test_route = tree[0]
-#test_route = ["TR","TL","S","CL"]
+
 box_num = 0 #represents number of delivered boxes - 1
 box_inc = 0 #0 for collecting, 1 for delivering
 RGB_inc = 1 #1 for YR, 2 for GB
@@ -86,9 +89,16 @@ directions = [robot.go_forward, robot.reverse]
 
 robot.servo.duty_u16(SERVO_0)
 previous_state, state_counter, current_f = robot.start(previous_state, F1_ORIGINAL, current_f, state_counter, LINE_CORRECTION, STATE_COUNTER_TRIP, 1)
-while (box_num < 9) and (button_stop != True):
+while (box_num < 9):
     cur = -1
     while cur < (len(test_route) - 1):
+        current_button_value = robot.button.value()
+        #print(current_button_value)
+        
+        if current_button_value == 1:
+            button_stop = True
+            break
+
         next_node = test_route[cur + 1]
         node = robot.detect_node()
 
@@ -96,17 +106,18 @@ while (box_num < 9) and (button_stop != True):
             cur_dir = 1 
         else:
             cur_dir = 0
-        #if (next_node == "B"):
-            #while((robot.distance_sensor.ping()-50) > 30):
-            #    previous_state, state_counter, current_f = directions[cur_dir](previous_state, F1_ORIGINAL, current_f, state_counter, LINE_CORRECTION, STATE_COUNTER_TRIP)
-            #print(next_node)
-            #cur += 1
-        #else:
+
         if node == False:
-            try:
-                previous_state, state_counter, current_f = directions[cur_dir](previous_state, F1_ORIGINAL, current_f, state_counter, LINE_CORRECTION, STATE_COUNTER_TRIP, 1)
-            except:
-                robot.reverse()
+            if (test_route[cur + 1] == "B"):
+                try:
+                    previous_state, state_counter, current_f = directions[cur_dir](previous_state, F1_ORIGINAL, current_f, state_counter, 0, STATE_COUNTER_TRIP, 1)
+                except:
+                    robot.reverse()
+            else:
+                try:
+                    previous_state, state_counter, current_f = directions[cur_dir](previous_state, F1_ORIGINAL, current_f, state_counter, LINE_CORRECTION, STATE_COUNTER_TRIP, 1)
+                except:
+                    robot.reverse()
         
         elif node == True:
             #print(next_node)
@@ -117,7 +128,7 @@ while (box_num < 9) and (button_stop != True):
                 previous_state = node_types[next_node](F3_ORIGINAL, previous_state)
             
             elif (test_route[cur + 1] == "BL") or (test_route[cur + 1] == "BR"):
-                   previous_state = node_types[next_node](F3_ORIGINAL, previous_state, box_num)
+                   previous_state = node_types[next_node](F3_ORIGINAL, previous_state)
 
             #elif ((test_route[cur + 2] == "B") and (cur < (len(test_route) - 1))):
                 #   node_types[next_node](F3_ORIGINAL)
@@ -128,16 +139,21 @@ while (box_num < 9) and (button_stop != True):
                 previous_state = node_types[next_node](F2_ORIGINAL, previous_state)
                 
             cur += 1
-    if (box_inc == 0): #fetching box
-        if (box_num == 8):
-            break
-        #RGB_inc = robot.get_box()
-        #print(RGB_inc) 
-        test_route = tree[4 * box_num + 2 * box_inc + RGB_inc]
-    elif (box_inc == 1): #dropping box or returning to home square
-        test_route = tree[4 * box_num + 2 * box_inc + RGB_inc]
-        box_num += 1
-    box_inc = abs(box_inc - 1)
+
+    if button_stop == True:
+        break
+    else:
+            
+        if (box_inc == 0): #fetching box
+            if (box_num == 8):
+                break
+            #RGB_inc = robot.get_box()
+            #print(RGB_inc) 
+            test_route = tree[4 * box_num + 2 * box_inc + RGB_inc]
+        elif (box_inc == 1): #dropping box or returning to home square
+            test_route = tree[4 * box_num + 2 * box_inc + RGB_inc]
+            box_num += 1
+        box_inc = abs(box_inc - 1)
 
 
 robot.led.value(0)
